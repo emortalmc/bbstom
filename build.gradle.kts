7583ae2a03ba6b9228861aad61b7baa40f8dc846
emortal <emortaldev@proton.me>
2026-07-15 23:20:54 +0100

    switch to hollowcube molang parser

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,12 @@ java.toolchain.languageVersion = JavaLanguageVersion.of(25)
 
 repositories {
     mavenCentral()
-    maven("https://maven.draylar.dev/releases") // arcane (molang)
 }
 
 dependencies {
     compileOnly("net.minestom:minestom:2025.10.31-1.21.10")
     implementation("com.alibaba.fastjson2:fastjson2:2.0.60")
-    implementation("dev.omega:arcane:0.1.4") // molang
+    implementation("dev.hollowcube:molang:1.1.0")
     implementation("org.joml:joml:1.10.8")
 }
 
--- src/main/java/dev/emortal/bbstom/BBDataPoint.java
+++ src/main/java/dev/emortal/bbstom/BBDataPoint.java
@@ -1,32 +1,15 @@
 package dev.emortal.bbstom;
 
 import com.alibaba.fastjson2.annotation.JSONCreator;
-import dev.omega.arcane.ast.ObjectAwareExpression;
-import dev.omega.arcane.exception.MolangLexException;
-import dev.omega.arcane.exception.MolangParseException;
-import dev.omega.arcane.parser.MolangParser;
-import dev.omega.arcane.reference.ExpressionBindingContext;
-import dev.omega.arcane.reference.ReferenceType;
+import net.hollowcube.molang.MolangExpr;
+import net.hollowcube.molang.eval.MolangEvaluator;
 import net.minestom.server.MinecraftServer;
 import org.joml.Vector3f;
 
+import java.util.Map;
+
 @SuppressWarnings("unused")
 public record BBDataPoint(Vector3f vector, String xExp, String yExp, String zExp) {
-    private static final ExpressionBindingContext ANIM_TIME_CONTEXT = ExpressionBindingContext.create();
-
-    static {
-        ANIM_TIME_CONTEXT.registerReferenceResolver(
-                ReferenceType.QUERY, // query.anim_time
-                "anim_time",
-                Float.class,
-                r -> new ObjectAwareExpression<>(r) {
-                    @Override
-                    public float evaluate() {
-                        return r;
-                    }
-                }
-        );
-    }
 
     @JSONCreator
     public BBDataPoint(String x, String y, String z) {
@@ -39,17 +22,17 @@ public record BBDataPoint(Vector3f vector, String xExp, String yExp, String zExp
         try {
             vec.setComponent(0, Float.parseFloat(x));
         } catch (NumberFormatException e) {
-            xExp = x.replaceAll("\n", "");
+            xExp = x.replace("\n", "");
         }
         try {
             vec.setComponent(1, Float.parseFloat(y));
         } catch (NumberFormatException e) {
-            yExp = y.replaceAll("\n", "");
+            yExp = y.replace("\n", "");
         }
         try {
             vec.setComponent(2, Float.parseFloat(z));
         } catch (NumberFormatException e) {
-            zExp = z.replaceAll("\n", "");
+            zExp = z.replace("\n", "");
         }
 
         this(vec, xExp, yExp, zExp);
@@ -59,19 +42,20 @@ public record BBDataPoint(Vector3f vector, String xExp, String yExp, String zExp
         Vector3f vec = new Vector3f(vector);
 
         try {
+            MolangEvaluator molangEvaluator = new MolangEvaluator(Map.of());
             if (!xExp.isEmpty()) {
-                float evaluate = MolangParser.parse(xExp).bind(ANIM_TIME_CONTEXT, time).evaluate();
-                vec.setComponent(0, evaluate);
+                MolangExpr molangExpr = MolangExpr.parseOrThrow(xExp);
+                vec.x = (float) molangEvaluator.eval(molangExpr);
             }
             if (!yExp.isEmpty()) {
-                float evaluate = MolangParser.parse(yExp).bind(ANIM_TIME_CONTEXT, time).evaluate();
-                vec.setComponent(1, evaluate);
+                MolangExpr molangExpr = MolangExpr.parseOrThrow(yExp);
+                vec.y = (float) molangEvaluator.eval(molangExpr);
             }
             if (!zExp.isEmpty()) {
-                float evaluate = MolangParser.parse(zExp).bind(ANIM_TIME_CONTEXT, time).evaluate();
-                vec.setComponent(2, evaluate);
+                MolangExpr molangExpr = MolangExpr.parseOrThrow(zExp);
+                vec.z = (float) molangEvaluator.eval(molangExpr);
             }
-        } catch (MolangLexException | MolangParseException e) {
+        } catch (Exception e) {
             MinecraftServer.getExceptionManager().handleException(e);
         }
 
